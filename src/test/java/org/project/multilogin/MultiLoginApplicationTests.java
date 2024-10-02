db9a0d7da55726a30c5e55bcfd20711ae7f0c076
Victor Ihedioha <ihedioha.victor@gmail.com>
2024-10-02 21:14:57 +0100

    Create Spring Boot Starter Project For Multi-login

---
+++ src/test/java/org/project/multilogin/MultiLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.multilogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
